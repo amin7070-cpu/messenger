7f3accc58917d03f0558656523a393fbc6700cc9
amin7070-cpu <amindridi801@gmail.com>
2024-11-10 11:53:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -276,5 +276,5 @@ def reject_invitation(connection_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='0.0.0.0', port=8000)
+    socketio.run(app, debug=True)
 
